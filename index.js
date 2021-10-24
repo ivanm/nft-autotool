08c66c678bd3d4d313fa929aff15b8cabb4e5edc
Ivan Mayoral <iimayoral@gmail.com>
2021-10-23 19:53:33 -0500

    Adding thumbnail processing

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const CONFIG_DIR = process.env.NFT_AUTOTOOL_CONFIG
 const OUTPUT_DIR = process.env.NFT_AUTOTOOL_OUTPUT
   ? process.env.NFT_AUTOTOOL_OUTPUT
   : "./output";
+const OUTPUT_THUMBNAILS_DIR = `${OUTPUT_DIR}/thumbnails`;
 const PARTS_DIR = `${CONFIG_DIR}/images`;
 const INITIAL_IMAGE_ID = 1;
 const charMap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -19,7 +20,8 @@ const {
     dnaLayerIds,
     collectionName,
     maxNumber,
-    extractDimensions
+    extractDimensions,
+    thumbnailsResize
   }
 } = require(`${CONFIG_DIR}/settings.js`);
 
@@ -77,43 +79,77 @@ const getTraitFromAttr = (attributes, type) =>
 console.log("Welcome! lets generate some images >:3");
 
 let database = {};
-const dynamicDnaLayers = layers.filter(({ dynamicDna }) => dynamicDna);
+let initialId = INITIAL_IMAGE_ID;
 
-let dynamicDnaOrder = [...dynamicDnaLayers];
-dynamicDnaOrder.sort((a, b) => a.dynamicDna.order - b.dynamicDna.order);
-dynamicDnaOrder = dynamicDnaOrder.map(({ name }) => name);
+try {
+  let rawDatabase = fs.readFileSync(`${OUTPUT_DIR}/database.json`);
+  database = JSON.parse(rawDatabase);
+  let keys = Object.keys(database).map(k => parseInt(k));
+  keys.sort((a, b) => a - b);
+  initialId = keys[keys.length - 1] + 1;
+} catch (e) {}
 
-let dynamicDnaMixes = new Array(dynamicDnaOrder.length).fill([]);
-dynamicDnaOrder.forEach((dynamicDnaName, index) => {
-  size = dynamicDnaLayers.find(({ name }) => name == dynamicDnaName).dynamicDna
-    .size;
+let restore = {};
+try {
+  let rawRestore = fs.readFileSync(`${OUTPUT_DIR}/restore.json`);
+  restore = JSON.parse(rawRestore);
+} catch (e) {}
+
+// Optional Dynamic Trait Generatios
+//
+// Layers can have different variations.
+// If you are going to use 3 different images for 'eyes1'
+// then you would need eyes1A, eyes1B,eyes1C, and use dynamicTrait with size 3
+// For multiple dynamic traits add order next to size
+// so the final generated name and metadata will keep this schema
+//
+const dynamicTraitLayers = layers.filter(({ dynamicTrait }) => dynamicTrait);
+let dynamicTraitOrder = [...dynamicTraitLayers];
+dynamicTraitOrder.sort((a, b) => a.dynamicTrait.order - b.dynamicTrait.order);
+dynamicTraitOrder = dynamicTraitOrder.map(({ name }) => name);
+
+let dynamicTraitMixes = new Array(dynamicTraitOrder.length).fill([]);
+dynamicTraitOrder.forEach((dynamicTraitName, index) => {
+  size = dynamicTraitLayers.find(({ name }) => name == dynamicTraitName)
+    .dynamicTrait.size;
   for (let i = 0; i < size; i++) {
-    dynamicDnaMixes[index] = [...dynamicDnaMixes[index], charMap[i]];
+    dynamicTraitMixes[index] = [...dynamicTraitMixes[index], charMap[i]];
   }
 });
 
-const dynamicCombinations = dynamicDnaMixes.length
-  ? cartesian(dynamicDnaMixes).map(el => ({
+const dynamicCombinations = dynamicTraitMixes.length
+  ? cartesian(dynamicTraitMixes).map(el => ({
       letters: el.join(""),
-      data: Object.assign(
+      dynamic: Object.assign(
         {},
-        ...Object.entries(Object.assign({}, dynamicDnaOrder)).map(([a, b]) => ({
-          [b]: el[parseInt(a)]
-        }))
+        ...Object.entries(Object.assign({}, dynamicTraitOrder)).map(
+          ([a, b]) => ({
+            [b]: el[parseInt(a)]
+          })
+        )
       )
     }))
-  : [{ letters: "", data: {} }];
+  : [{ letters: "", dynamic: {} }];
 
-for (let i = INITIAL_IMAGE_ID; i <= maxNumber; i++) {
+for (let i = initialId; i < initialId + maxNumber; i++) {
   console.log(`Calculating #${i}`);
 
   let uniqueDnaChunk = "";
-  let dynamicDna = [];
+  let dynamicTrait = [];
   let layerIds = [];
   do {
-    dynamicDna = [randomAbc(), randomAbc()];
+    dynamicTrait = [randomAbc(), randomAbc()];
 
     layerIds = layers.map(({ items }) => randomChance(items).id);
+    if (restore[i]) {
+      layerIds = layers.map(({ label, items }) => {
+        const trait = restore[i].attributes.find(
+          ({ trait_type }) => trait_type === label
+        );
+        const item = items.find(({ name }) => name === trait.value);
+        return item.id;
+      });
+    }
 
     uniqueDnaChunk = dnaLayerIds
       .map(dnaLayerId => layerIds[dnaLayerId])
@@ -144,11 +180,17 @@ for (let i = INITIAL_IMAGE_ID; i <= maxNumber; i++) {
     }
   } while (!uniqueDnaChunk);
 
-  dynamicCombinations.forEach(({ letters, data }) => {
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  }
+
+  dynamicCombinations.forEach(({ letters, dynamic }) => {
     sharp(
       getPart(
         layers[0].name,
-        `${layerIds[0]}${data[layers[0].name] ? data[layers[0].name] : ""}`
+        `${layerIds[0]}${
+          dynamic[layers[0].name] ? dynamic[layers[0].name] : ""
+        }`
       )
     )
       .composite(
@@ -156,7 +198,7 @@ for (let i = INITIAL_IMAGE_ID; i <= maxNumber; i++) {
           input: getPart(
             layers[index].name,
             `${layerId}${
-              data[layers[index].name] ? data[layers[index].name] : ""
+              dynamic[layers[index].name] ? dynamic[layers[index].name] : ""
             }`
           ),
           gravity: "southeast"
@@ -165,7 +207,7 @@ for (let i = INITIAL_IMAGE_ID; i <= maxNumber; i++) {
       .toBuffer({ resolveWithObject: true })
       .then(({ data, info }) => {
         console.log(`Finished composing #${i}${letters}`);
-        sharp(data)
+        const sharpImg = sharp(data)
           .extract(extractDimensions)
           .toFile(`${OUTPUT_DIR}/${i}${letters}.png`, function(err) {
             console.log(`Writing ${i}${letters}.png`);
@@ -173,6 +215,25 @@ for (let i = INITIAL_IMAGE_ID; i <= maxNumber; i++) {
               console.log(`Error: ${err}`);
             }
           });
+        if (thumbnailsResize) {
+          if (!fs.existsSync(OUTPUT_THUMBNAILS_DIR)) {
+            fs.mkdirSync(OUTPUT_THUMBNAILS_DIR);
+          }
+
+          sharpImg
+            .resize(thumbnailsResize)
+            .toFile(
+              `${OUTPUT_DIR}/thumbnails/${i}${letters}-${thumbnailsResize.width}.png`,
+              function(err) {
+                console.log(
+                  `Writing ${i}${letters}-${thumbnailsResize.width}.png`
+                );
+                if (err) {
+                  console.log(`Error: ${err}`);
+                }
+              }
+            );
+        }
       });
   });
 }
